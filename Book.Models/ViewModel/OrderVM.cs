5fa91bee40340977b357f6a46c2f46c0e167ff74
Mohamed Elsayed <86891363+Mohamedzonkol@users.noreply.github.com>
2024-05-04 03:20:41 +0300

    Add files via upload

---
+++ Book.Models/ViewModel/OrderVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Book.Models.ViewModel
+{
+	public class OrderVM
+	{
+
+		public OrderHeader OrderHeader { get; set; }
+		public IEnumerable<OrderDetail> OrderDetail { get; set; }
+	}
+}
---
+++ Book.Models/ViewModel/ShoppingCartVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Book.Models.ViewModel
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
